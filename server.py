77338df10ccc1402aa8ade4901ac61674f660bd6
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-03-18 14:42:59 -0400

    Testing

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/index')
+def home(name=None):
+    return render_template('index.html',name=name)
+
+@app.route('/about')
+def about(name=None):
+    return render_template('about.html',name=name)
+
