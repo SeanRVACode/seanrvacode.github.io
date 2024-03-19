63c7b4da658a04e8797e82e5c8f2967e84db917c
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-03-18 21:06:00 -0400

    Website stuff

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
 
 app = Flask(__name__)
 
@@ -10,3 +10,16 @@ def home(name=None):
 def about(name=None):
     return render_template('about.html',name=name)
 
+@app.route("/images/<img>")
+def images(img):
+    return redirect(f'/static/images/{img}')
+
+@app.route('/assets/css/<file>')
+def css(file):
+    return redirect(f'/static/assets/css/{file}')
+@app.route('/assets/js/<file>')
+def js(file):
+    return redirect(f'/static/assets/js/{file}')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return redirect('/static/favicon.ico')
