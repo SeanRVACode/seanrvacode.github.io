25d1f96b70d2aa3fe26290be63c416776a5c38fa
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-07-01 19:26:31 -0400

    Fixes

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def home():
     return render_template('index.html',thumb_path=thumb_path,full_path=full_path)
 
 # Handles the redirect for thumbnail images so I don't have to type url_for a ton
-@app.route('/assets/img/portfolio/thumbnails/<img>')
+@app.route('assets/img/portfolio/thumbnails/<img>')
 def thumbnails(img):
     return redirect(f'/static/assets/portfolio/thumbnails/{img}')
 
-@app.route('/assets/img/portfolio/fullsize/<img>')
+@app.route('assets/img/portfolio/fullsize/<img>')
 def fullsize(img):
     return redirect(f'/static/assets/portfolio/fullsize/img')
