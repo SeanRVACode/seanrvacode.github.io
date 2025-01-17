2da8ba6f33dd56fb116118c5dd214055d250cc2d
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-01-17 18:45:30 -0500

    Add tax return status checker and update functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import date
-from flask import Flask, abort, render_template, redirect,url_for,flash
+from flask import Flask, abort, render_template, redirect,url_for,flash,jsonify
 from flask_bootstrap import Bootstrap5
+import random
 
 app = Flask(__name__)
 Bootstrap5(app)
@@ -19,3 +20,13 @@ def thumbnails(img):
 @app.route('assets/img/portfolio/fullsize/<img>')
 def fullsize(img):
     return redirect(f'/static/assets/portfolio/fullsize/img')
+
+@app.route('/returnChecker')
+def taxReturnChecker():
+
+    return render_template('taxReturnChecker.html',status="In Progress")
+@app.route('/getStatus')
+def getStatus():
+    status_messages = ['How fast do these people expect us to get this done? We are working on it!','In the Dumpster on Fire','Sent to the wrong Client','Still waiting for you to sign that Engagement Letter.']
+    status = random.choice(status_messages)
+    return jsonify(status=status)
